b0479454fdc61a07228afd7a2c8a24c98218d4a8
it-hwang <hitn8757@gmail.com>
2021-07-30 21:36:42 +0900

    Add hal/rvpb/Uart.c

---
+++ hal/rvpb/Uart.c
@@ -0,0 +1,20 @@
+#include "stdint.h"
+#include "Uart.h"
+#include "HalUart.h"
+
+extern volatile PL011t_* Uart;
+
+void Hal_uart_init(void)
+{
+    // Enable UART
+    Uart->uartcr.bits.UARTEN = 0;
+    Uart->uartcr.bits.TXE = 1;
+    Uart->uartcr.bits.RXE = 1;
+    Uart->uartct.bits.UARTEN = 1;
+}
+
+void Hal_uart_put_char(uint8_t ch)
+{
+    while(Uart->uartfr.bits.TXFF);
+    Uart->uartdr.all = (ch & 0xFF);
+}
