bdc43d871fbdd7937516043628143d177f036128
it-hwang <hitn8757@gmail.com>
2021-07-31 21:13:52 +0900

    Add Hal_uart_get_char()

--- hal/rvpb/Uart.c
+++ hal/rvpb/Uart.c
@@ -18,3 +18,25 @@ void Hal_uart_put_char(uint8_t ch)
     while(Uart->uartfr.bits.TXFF);
     Uart->uartdr.all = (ch & 0xFF);
 }
+
+uint8_t Hal_uart_get_char(void)
+{
+    uint8_t data;
+
+    while(Uart->uartfr.bits.RXFE);
+
+    // Check the error flag
+    if (Uart->uartdr.bits.BE || Uart->uartdr.bits.FE ||
+        Uart->uartdr.bits.OE || Uart->uartdr.bits.PE)
+    {
+        //Clear the error
+        Uart->uartrsr.bits.BE = 1;
+        Uart->uartrsr.bits.FE = 1;
+        Uart->uartrsr.bits.OE = 1;
+        Uart->uartrsr.bits.PE = 1;
+        return 0;
+    }
+
+    data = Uart->uartdr.bits.DATA;
+    return data;
+}
