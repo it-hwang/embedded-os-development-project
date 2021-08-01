969f449c8792f0fa03f18c438c2da10b6a9eb203
it-hwang <hitn8757@gmail.com>
2021-08-01 14:35:39 +0900

    Add vsprintf() definition

--- lib/stdio.h
+++ lib/stdio.h
@@ -1,7 +1,10 @@
 #ifndef LIB_STDIO_H
 #define LIB_STDIO_H
 
+#include "stdarg.h"
+
 uint32_t putstr(const char* s);
 uint32_t debug_printf(const char* format, ...);
+uint32_t vsprintf(char* buf, const char* format, va_list arg);
 
 #endif /* LIB_STDIO_H */
